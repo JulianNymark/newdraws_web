93627b60c28b959e9c66bf880c467d3c0e653e09
j <j@m.localhost>
2018-01-29 23:40:26 +0100

    unregister serviceworker :^) :poop:

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
+import * as ServiceWorker from './registerServiceWorker';
 
 import * as Utils from './utils';
 import './App.css';
 import { Draws } from './presentational/draws';
 import { Store } from './types';
+
+ServiceWorker.unregister();
 
 const logo = require('./logo.svg');
 
